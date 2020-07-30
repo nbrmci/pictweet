ca9d4eeaac7009964890bcc8afc699a3608736c1
nbrmci <cr3886165@gmail.com>
2020-07-30 19:14:43 +0900

    edit file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to tweet_path(params[:tweet_id])  }
       format.json
+    end
   end
 
   private
